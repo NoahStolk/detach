94973a065bb77b66321d5247b822dc0ac924af48
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-12-10 21:02:13 +0100

    Rewrite collisions (WIP)

---
+++ src/Detach/Collisions/Primitives/LineSegment2D.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace Detach.Collisions.Primitives;
+
+public struct LineSegment2D
+{
+	public Vector2 Start;
+	public Vector2 End;
+
+	public LineSegment2D(Vector2 start, Vector2 end)
+	{
+		Start = start;
+		End = end;
+	}
+
+	public float Length => Vector2.Distance(Start, End);
+	public float LengthSquared => Vector2.DistanceSquared(Start, End);
+}
