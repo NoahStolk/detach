34a9d5059c7a817a092065b65c92bd014010e02c
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-12-12 21:25:41 +0100

    Implement PointInOrientedRectangle

--- src/Detach/Collisions/Primitives/OrientedRectangle.cs
+++ src/Detach/Collisions/Primitives/OrientedRectangle.cs
@@ -6,12 +6,12 @@ public struct OrientedRectangle
 {
 	public Vector2 Position;
 	public Vector2 HalfExtents;
-	public float Rotation;
+	public float RotationInRadians;
 
-	public OrientedRectangle(Vector2 position, Vector2 halfExtents, float rotation)
+	public OrientedRectangle(Vector2 position, Vector2 halfExtents, float rotationInRadians)
 	{
 		Position = position;
 		HalfExtents = halfExtents;
-		Rotation = rotation;
+		RotationInRadians = rotationInRadians;
 	}
 }
