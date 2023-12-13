3a30265c9a15b06f9ca589aabe05cc7d3434f8dc
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-12-13 20:29:50 +0100

    Implement 3D primitives

---
+++ src/Detach/Collisions/Primitives/Obb.cs
@@ -0,0 +1,18 @@
+﻿using Detach.Numerics;
+using System.Numerics;
+
+namespace Detach.Collisions.Primitives;
+
+public record struct Obb
+{
+	public Vector3 Position;
+	public Vector3 Size;
+	public Matrix3 Orientation;
+
+	public Obb(Vector3 position, Vector3 size, Matrix3 orientation)
+	{
+		Position = position;
+		Size = size;
+		Orientation = orientation;
+	}
+}
---
+++ src/Detach/Collisions/Primitives/Ray.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace Detach.Collisions.Primitives;
+
+public record struct Ray
+{
+	public Vector3 Origin;
+	public Vector3 Direction;
+
+	public Ray(Vector3 origin, Vector3 direction)
+	{
+		Origin = origin;
+		Direction = Vector3.Normalize(direction);
+	}
+
+	public static Ray FromPoints(Vector3 start, Vector3 end)
+	{
+		return new Ray(start, end - start);
+	}
+}
