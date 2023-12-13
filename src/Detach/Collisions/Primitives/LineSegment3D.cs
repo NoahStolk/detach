449856595f850c207b7779173beeb5093a0b3f90
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-12-13 20:29:50 +0100

    Implement 3D primitives

---
+++ src/Detach/Collisions/Primitives/LineSegment3D.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace Detach.Collisions.Primitives;
+
+public record struct LineSegment3D
+{
+	public Vector3 Start;
+	public Vector3 End;
+
+	public LineSegment3D(Vector3 start, Vector3 end)
+	{
+		Start = start;
+		End = end;
+	}
+
+	public float Length => Vector3.Distance(Start, End);
+	public float LengthSquared => Vector3.DistanceSquared(Start, End);
+}
