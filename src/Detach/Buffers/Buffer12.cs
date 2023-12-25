e394706e1bddd1a27478f078eeb970fa326b91f3
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-12-25 23:53:30 +0100

    Display all Buffer elements in debugger

--- src/Detach/Buffers/Buffer12.cs
+++ src/Detach/Buffers/Buffer12.cs
@@ -1,9 +1,15 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace Detach.Buffers;
 
 [InlineArray(12)]
+[DebuggerDisplay("Items = {Items}")]
 public struct Buffer12<T>
 {
+	[DebuggerBrowsable(DebuggerBrowsableState.Never)]
 	private T _0;
+
+	[DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+	internal T[] Items => this[..12].ToArray();
 }
--- src/Detach/Buffers/Buffer6.cs
+++ src/Detach/Buffers/Buffer6.cs
@@ -1,9 +1,15 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace Detach.Buffers;
 
 [InlineArray(6)]
+[DebuggerDisplay("Items = {Items}")]
 public struct Buffer6<T>
 {
+	[DebuggerBrowsable(DebuggerBrowsableState.Never)]
 	private T _0;
+
+	[DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+	internal T[] Items => this[..6].ToArray();
 }
