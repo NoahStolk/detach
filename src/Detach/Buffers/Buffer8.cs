2b4481c52bc3a6f4020a90799b866be786e294ff
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-12-25 23:53:30 +0100

    Display all Buffer elements in debugger

--- src/Detach/Buffers/Buffer8.cs
+++ src/Detach/Buffers/Buffer8.cs
@@ -1,9 +1,15 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace Detach.Buffers;
 
 [InlineArray(8)]
+[DebuggerDisplay("Items = {Items}")]
 public struct Buffer8<T>
 {
+	[DebuggerBrowsable(DebuggerBrowsableState.Never)]
 	private T _0;
+
+	[DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+	internal T[] Items => this[..8].ToArray();
 }
