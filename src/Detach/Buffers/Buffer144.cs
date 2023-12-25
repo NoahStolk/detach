a8a0fa6430c32dadf3bac49a4d3d076febe2395c
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-12-25 13:56:24 +0100

    Fix bugs

--- src/Detach/Buffers/Buffer144.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace Detach.Buffers;
-
-[InlineArray(144)]
-public struct Buffer144<T>
-{
-	private T _0;
-}
--- src/Detach/Collisions/CollisionManifold.cs
+++ src/Detach/Collisions/CollisionManifold.cs
@@ -5,9 +5,18 @@ namespace Detach.Collisions;
 
 public struct CollisionManifold
 {
+	// TODO: Refactor and use out parameters instead of bool field.
 	public bool Colliding;
 	public Vector3 Normal;
 	public float Depth;
 	public int ContactCount;
-	public Buffer144<Vector3> Contacts;
+	public Buffer24<Vector3> Contacts;
+
+	public static CollisionManifold Empty => new()
+	{
+		Colliding = false,
+		Normal = Vector3.UnitZ,
+		Depth = float.MaxValue,
+		ContactCount = 0,
+	};
 }
