0206a0b9ca05cccf5a831091247254fbad009463
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-01-31 21:30:52 +0100

    Implement rendering 2D primitives

--- src/Detach.VisualTests/Ui/MainWindow.cs
+++ src/Detach.VisualTests/Ui/MainWindow.cs
@@ -1,14 +1,51 @@
-﻿using ImGuiNET;
+﻿using Detach.Collisions.Primitives;
+using ImGuiNET;
+using System.Numerics;
 
 namespace Detach.VisualTests.Ui;
 
 public static class MainWindow
 {
+	private static readonly List<LineSegment2D> _lineSegments = [];
+	private static readonly List<Circle> _circles = [];
+	private static readonly List<Rectangle> _rectangles = [];
+	private static readonly List<OrientedRectangle> _orientedRectangles = [];
+
 	public static void Render()
 	{
-		if (ImGui.Begin("Main Window"))
+		if (ImGui.Begin("Main Window", ImGuiWindowFlags.NoCollapse))
 		{
-			ImGui.Text("Hello, world!");
+			LineSegment2DPopup.Render(_lineSegments);
+			if (ImGui.Button("New line segment"))
+				ImGui.OpenPopup(LineSegment2DPopup.PopupName);
+
+			CirclePopup.Render(_circles);
+			if (ImGui.Button("New circle"))
+				ImGui.OpenPopup(CirclePopup.PopupName);
+
+			RectanglePopup.Render(_rectangles);
+			if (ImGui.Button("New rectangle"))
+				ImGui.OpenPopup(RectanglePopup.PopupName);
+
+			OrientedRectanglePopup.Render(_orientedRectangles);
+			if (ImGui.Button("New oriented rectangle"))
+				ImGui.OpenPopup(OrientedRectanglePopup.PopupName);
+
+			ImDrawListPtr drawList = ImGui.GetWindowDrawList();
+			Vector2 origin = ImGui.GetCursorScreenPos();
+			PositionedDrawList positionedDrawList = new(drawList, origin);
+
+			foreach (LineSegment2D lineSegment in _lineSegments)
+				positionedDrawList.AddLine(lineSegment, 0xFFFFFFFF);
+
+			foreach (Circle circle in _circles)
+				positionedDrawList.AddCircle(circle, 0xFFFFFFFF);
+
+			foreach (Rectangle rectangle in _rectangles)
+				positionedDrawList.AddRectangle(rectangle, 0xFFFFFFFF);
+
+			foreach (OrientedRectangle orientedRectangle in _orientedRectangles)
+				positionedDrawList.AddOrientedRectangle(orientedRectangle, 0xFFFFFFFF);
 		}
 
 		ImGui.End();
---
+++ src/Detach/Buffers/Buffer4.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Detach.Buffers;
+
+[InlineArray(4)]
+[DebuggerDisplay("Items = {Items}")]
+public struct Buffer4<T>
+{
+	[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+	private T _0;
+
+	[DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+	internal T[] Items => this[..4].ToArray();
+}
