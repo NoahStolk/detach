360e423513c435ec8551116b62d49ebc51569641
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-06-02 12:05:46 +0200

    Update Geometry2DTests.cs

--- src/Detach.Tests/Tests/Collisions/Geometry2DTests.cs
+++ src/Detach.Tests/Tests/Collisions/Geometry2DTests.cs
@@ -45,4 +45,76 @@ public class Geometry2DTests
 		Assert.IsFalse(Geometry2D.PointOnLine(new Vector2(5, 6), lineDiagonal));
 		Assert.IsFalse(Geometry2D.PointOnLine(new Vector2(0, -0.5f), lineDiagonal));
 	}
+
+	[TestMethod]
+	public void PointInCircle()
+	{
+		Circle circleCenter = new(new Vector2(0, 0), 5);
+		Assert.IsTrue(Geometry2D.PointInCircle(new Vector2(0, 0), circleCenter));
+		Assert.IsTrue(Geometry2D.PointInCircle(new Vector2(3, 4), circleCenter));
+		Assert.IsTrue(Geometry2D.PointInCircle(new Vector2(5, 0), circleCenter));
+		Assert.IsTrue(Geometry2D.PointInCircle(new Vector2(0, 5), circleCenter));
+		Assert.IsFalse(Geometry2D.PointInCircle(new Vector2(6, 0), circleCenter));
+		Assert.IsFalse(Geometry2D.PointInCircle(new Vector2(0, 6), circleCenter));
+		Assert.IsFalse(Geometry2D.PointInCircle(new Vector2(6, 6), circleCenter));
+		Assert.IsFalse(Geometry2D.PointInCircle(new Vector2(0, -6), circleCenter));
+		Assert.IsFalse(Geometry2D.PointInCircle(new Vector2(-6, 0), circleCenter));
+		Assert.IsFalse(Geometry2D.PointInCircle(new Vector2(-6, -6), circleCenter));
+
+		Circle circleOffset = new(new Vector2(5, 5), 5);
+		Assert.IsTrue(Geometry2D.PointInCircle(new Vector2(5, 5), circleOffset));
+		Assert.IsTrue(Geometry2D.PointInCircle(new Vector2(8, 9), circleOffset));
+		Assert.IsTrue(Geometry2D.PointInCircle(new Vector2(10, 5), circleOffset));
+		Assert.IsTrue(Geometry2D.PointInCircle(new Vector2(5, 10), circleOffset));
+		Assert.IsFalse(Geometry2D.PointInCircle(new Vector2(11, 5), circleOffset));
+		Assert.IsFalse(Geometry2D.PointInCircle(new Vector2(5, 11), circleOffset));
+		Assert.IsFalse(Geometry2D.PointInCircle(new Vector2(11, 11), circleOffset));
+		Assert.IsFalse(Geometry2D.PointInCircle(new Vector2(5, -1), circleOffset));
+		Assert.IsFalse(Geometry2D.PointInCircle(new Vector2(-1, 5), circleOffset));
+		Assert.IsFalse(Geometry2D.PointInCircle(new Vector2(-1, -1), circleOffset));
+	}
+
+	[TestMethod]
+	public void PointInRectangle()
+	{
+		Rectangle rectangleCenter = new(new Vector2(0, 0), new Vector2(5, 5));
+		Assert.IsTrue(Geometry2D.PointInRectangle(new Vector2(0, 0), rectangleCenter));
+		Assert.IsTrue(Geometry2D.PointInRectangle(new Vector2(3, 3), rectangleCenter));
+		Assert.IsTrue(Geometry2D.PointInRectangle(new Vector2(5, 5), rectangleCenter));
+		Assert.IsTrue(Geometry2D.PointInRectangle(new Vector2(0, 5), rectangleCenter));
+		Assert.IsFalse(Geometry2D.PointInRectangle(new Vector2(6, 0), rectangleCenter));
+		Assert.IsFalse(Geometry2D.PointInRectangle(new Vector2(0, 6), rectangleCenter));
+		Assert.IsFalse(Geometry2D.PointInRectangle(new Vector2(6, 6), rectangleCenter));
+		Assert.IsFalse(Geometry2D.PointInRectangle(new Vector2(0, -1), rectangleCenter));
+		Assert.IsFalse(Geometry2D.PointInRectangle(new Vector2(-1, 0), rectangleCenter));
+		Assert.IsFalse(Geometry2D.PointInRectangle(new Vector2(-1, -1), rectangleCenter));
+
+		Rectangle rectangleOffset = new(new Vector2(5, 5), new Vector2(5, 5));
+		Assert.IsTrue(Geometry2D.PointInRectangle(new Vector2(5, 5), rectangleOffset));
+		Assert.IsTrue(Geometry2D.PointInRectangle(new Vector2(8, 9), rectangleOffset));
+		Assert.IsTrue(Geometry2D.PointInRectangle(new Vector2(10, 5), rectangleOffset));
+		Assert.IsTrue(Geometry2D.PointInRectangle(new Vector2(5, 10), rectangleOffset));
+		Assert.IsFalse(Geometry2D.PointInRectangle(new Vector2(11, 5), rectangleOffset));
+		Assert.IsFalse(Geometry2D.PointInRectangle(new Vector2(5, 11), rectangleOffset));
+		Assert.IsFalse(Geometry2D.PointInRectangle(new Vector2(11, 11), rectangleOffset));
+		Assert.IsFalse(Geometry2D.PointInRectangle(new Vector2(5, -1), rectangleOffset));
+		Assert.IsFalse(Geometry2D.PointInRectangle(new Vector2(-1, 5), rectangleOffset));
+		Assert.IsFalse(Geometry2D.PointInRectangle(new Vector2(-1, -1), rectangleOffset));
+	}
+
+	[TestMethod]
+	public void PointInOrientedRectangle()
+	{
+		OrientedRectangle orientedRectangle = new(new Vector2(0, 0), new Vector2(5, 5), MathF.PI / 4);
+		Assert.IsTrue(Geometry2D.PointInOrientedRectangle(new Vector2(0, 0), orientedRectangle));
+		Assert.IsTrue(Geometry2D.PointInOrientedRectangle(new Vector2(3, 3), orientedRectangle));
+		//Assert.IsTrue(Geometry2D.PointInOrientedRectangle(new Vector2(5, 5), orientedRectangle));
+		Assert.IsTrue(Geometry2D.PointInOrientedRectangle(new Vector2(0, 5), orientedRectangle));
+		//Assert.IsFalse(Geometry2D.PointInOrientedRectangle(new Vector2(6, 0), orientedRectangle));
+		//Assert.IsFalse(Geometry2D.PointInOrientedRectangle(new Vector2(0, 6), orientedRectangle));
+		Assert.IsFalse(Geometry2D.PointInOrientedRectangle(new Vector2(6, 6), orientedRectangle));
+		//Assert.IsFalse(Geometry2D.PointInOrientedRectangle(new Vector2(0, -1), orientedRectangle));
+		//Assert.IsFalse(Geometry2D.PointInOrientedRectangle(new Vector2(-1, 0), orientedRectangle));
+		//Assert.IsFalse(Geometry2D.PointInOrientedRectangle(new Vector2(-1, -1), orientedRectangle));
+	}
 }
