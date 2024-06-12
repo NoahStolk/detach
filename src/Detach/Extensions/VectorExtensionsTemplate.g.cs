fc174d0eddc3806cf8a4abe1e233995b490605b3
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-06-12 20:46:45 +0200

    Generate inline array Buffer structs with T4

--- src/Detach/Extensions/VectorExtensionsTemplate.g.cs
+++ src/Detach/Extensions/VectorExtensionsTemplate.g.cs
@@ -4,6 +4,8 @@
 // Changes to this file may cause incorrect behavior and will be lost if the code is regenerated.
 // </auto-generated>
 
+#nullable enable
+
 using Detach.Numerics;
 using System.Numerics;
 
