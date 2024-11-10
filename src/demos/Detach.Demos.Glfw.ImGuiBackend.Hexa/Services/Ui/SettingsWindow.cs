9345ec32ef651a4798cc3f4d33531261df6f2b7c
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-11-10 17:58:11 +0100

    Add ImGui backend using Hexa bindings

---
+++ src/demos/Detach.Demos.Glfw.ImGuiBackend.Hexa/Services/Ui/SettingsWindow.cs
@@ -0,0 +1,26 @@
+﻿using Hexa.NET.ImGui;
+
+namespace Detach.Demos.Glfw.ImGuiBackend.Hexa.Services.Ui;
+
+public sealed class SettingsWindow
+{
+	private readonly Silk.NET.GLFW.Glfw _glfw;
+
+	private bool _vsync;
+
+	public SettingsWindow(Silk.NET.GLFW.Glfw glfw)
+	{
+		_glfw = glfw;
+	}
+
+	public void Render()
+	{
+		if (ImGui.Begin("Settings"))
+		{
+			if (ImGui.Checkbox("VSync", ref _vsync))
+				_glfw.SwapInterval(_vsync ? 1 : 0);
+		}
+
+		ImGui.End();
+	}
+}
