1684a0c3ba1d1e04a5e1cf6594fc93f45e06b332
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-11-10 21:35:06 +0100

    Add GlExtensions library

---
+++ src/Detach.GlExtensions/GlExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Silk.NET.OpenGL;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace Detach.GlExtensions;
+
+public static class GlExtensions
+{
+	// ReSharper disable once InconsistentNaming
+	public static void UniformMatrix4x4(this GL gl, int uniformLocation, Matrix4x4 value)
+	{
+		Span<float> data =
+		[
+			value.M11, value.M12, value.M13, value.M14,
+			value.M21, value.M22, value.M23, value.M24,
+			value.M31, value.M32, value.M33, value.M34,
+			value.M41, value.M42, value.M43, value.M44,
+		];
+		gl.UniformMatrix4(uniformLocation, 1, false, data);
+	}
+
+	public static void CheckError(this GL gl, [CallerMemberName] string caller = "", [CallerLineNumber] int line = 0)
+	{
+		GLEnum error = gl.GetError();
+		if (error != GLEnum.NoError)
+			throw new InvalidOperationException($"OpenGL error '{error}'. First noted in '{caller}' on line {line}.");
+	}
+}
