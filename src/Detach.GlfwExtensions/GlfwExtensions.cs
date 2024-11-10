486fbcb3404c573985fe42a528419e6eb6c5e15f
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-11-10 20:34:14 +0100

    Improve GlfwExtensions CheckError

--- src/Detach.GlfwExtensions/GlfwExtensions.cs
+++ src/Detach.GlfwExtensions/GlfwExtensions.cs
@@ -1,20 +1,24 @@
 ﻿using Silk.NET.GLFW;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Detach.GlfwExtensions;
 
 public static class GlfwExtensions
 {
-	public static unsafe void CheckError(this Glfw glfw)
+	public static unsafe void CheckError(this Glfw glfw, [CallerMemberName] string caller = "", [CallerLineNumber] int line = 0)
 	{
 		ErrorCode errorCode = glfw.GetError(out byte* c);
-		if (errorCode == ErrorCode.NoError || c == (byte*)0)
+		if (errorCode == ErrorCode.NoError)
 			return;
+
+		if (c == (byte*)0)
+			throw new InvalidOperationException($"GLFW error '{errorCode}'. First noted in '{caller}' on line {line}. No error message was given.");
 
 		StringBuilder errorBuilder = new();
 		while (*c != 0x00)
 			errorBuilder.Append((char)*c++);
 
-		throw new InvalidOperationException($"GLFW {errorCode}: {errorBuilder}");
+		throw new InvalidOperationException($"GLFW error '{errorCode}'. First noted in '{caller}' on line {line}. Error message: {errorBuilder}");
 	}
 }
