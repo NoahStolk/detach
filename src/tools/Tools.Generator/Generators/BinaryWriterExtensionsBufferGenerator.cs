b2de6cbf90b39eb7396a3d2326af669c99332981
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-12-11 00:29:13 +0100

    Generate BinaryWriter extensions for inline buffer structs

---
+++ src/tools/Tools.Generator/Generators/BinaryWriterExtensionsBufferGenerator.cs
@@ -0,0 +1,58 @@
+﻿using Tools.Generator.Internals;
+
+namespace Tools.Generator.Generators;
+
+internal sealed class BinaryWriterExtensionsBufferGenerator : IGenerator
+{
+	private readonly string[] _typeNames =
+	[
+		"sbyte",
+		"byte",
+		"short",
+		"ushort",
+		"int",
+		"uint",
+		"long",
+		"ulong",
+		"Half",
+		"float",
+		"double",
+	];
+
+	public string Generate()
+	{
+		CodeWriter codeWriter = new();
+
+		codeWriter.WriteLine(GeneratorConstants.Header);
+		codeWriter.WriteLine();
+		codeWriter.WriteLine("using Detach.Buffers;");
+		codeWriter.WriteLine("using Detach.Numerics;");
+		codeWriter.WriteLine();
+		codeWriter.WriteLine("namespace Detach.Extensions;");
+		codeWriter.WriteLine();
+		codeWriter.WriteLine("public static partial class BinaryWriterExtensions");
+		codeWriter.StartBlock();
+
+		foreach (int bufferSize in BufferConstants.Sizes)
+		{
+			foreach (string builtInTypeName in _typeNames)
+			{
+				string bufferTypeName = $"Buffer{bufferSize}<{builtInTypeName}>";
+
+				codeWriter.WriteLine($"public static void Write(this BinaryWriter binaryWriter, {bufferTypeName} buffer)");
+				codeWriter.StartBlock();
+				codeWriter.WriteLine($"for (int i = 0; i < {bufferTypeName}.Size; i++)");
+				codeWriter.StartIndent();
+				codeWriter.WriteLine("binaryWriter.Write(buffer[i]);");
+				codeWriter.EndIndent();
+				codeWriter.EndBlock();
+
+				codeWriter.WriteLine();
+			}
+		}
+
+		codeWriter.EndBlock();
+
+		return codeWriter.ToString();
+	}
+}
