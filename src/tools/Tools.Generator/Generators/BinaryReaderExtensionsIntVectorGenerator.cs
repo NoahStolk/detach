c6221a2a5a3dfc19c12210a7e3f24d7f67bc1bb6
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-12-10 22:19:30 +0100

    Implement BinaryReaderExtensionsIntVectorGenerator

---
+++ src/tools/Tools.Generator/Generators/BinaryReaderExtensionsIntVectorGenerator.cs
@@ -0,0 +1,53 @@
+﻿using Tools.Generator.Internals;
+
+namespace Tools.Generator.Generators;
+
+internal sealed class BinaryReaderExtensionsIntVectorGenerator
+{
+	private readonly (string BuiltInTypeName, string MethodName, string ReaderMethodName)[] _typeNames =
+	[
+		("sbyte", "Int8", "ReadSByte"),
+		("byte", "UInt8", "ReadByte"),
+		("short", "Int16", "ReadInt16"),
+		("ushort", "UInt16", "ReadUInt16"),
+		("int", "Int32", "ReadInt32"),
+		("uint", "UInt32", "ReadUInt32"),
+		("long", "Int64", "ReadInt64"),
+		("ulong", "UInt64", "ReadUInt64"),
+	];
+
+	public string Generate()
+	{
+		CodeWriter codeWriter = new();
+
+		codeWriter.WriteLine(GeneratorConstants.Header);
+		codeWriter.WriteLine();
+		codeWriter.WriteLine("using Detach.Numerics;");
+		codeWriter.WriteLine();
+		codeWriter.WriteLine("namespace Detach.Extensions;");
+		codeWriter.WriteLine();
+		codeWriter.WriteLine("public static partial class BinaryReaderExtensions");
+		codeWriter.StartBlock();
+
+		for (int i = 2; i <= 4; i++)
+		{
+			string intVectorTypeName = $"IntVector{i}";
+
+			foreach ((string builtInTypeName, string methodName, string readerMethodName) in _typeNames)
+			{
+				string readIntMethodName = $"Read{intVectorTypeName}Of{methodName}";
+
+				codeWriter.WriteLine($"public static {intVectorTypeName}<{builtInTypeName}> {readIntMethodName}(this BinaryReader binaryReader)");
+				codeWriter.StartBlock();
+				codeWriter.WriteLine($"return new {intVectorTypeName}<{builtInTypeName}>({string.Join(", ", Enumerable.Range(0, i).Select(_ => $"binaryReader.{readerMethodName}()"))});");
+				codeWriter.EndBlock();
+
+				codeWriter.WriteLine();
+			}
+		}
+
+		codeWriter.EndBlock();
+
+		return codeWriter.ToString();
+	}
+}
---
+++ src/tools/Tools.Generator/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tools.Generator.Generators;
+
+const string baseDirectory = @"C:\Users\NOAH\source\repos\detach\src\libs\Detach";
+
+BinaryReaderExtensionsIntVectorGenerator binaryReaderExtensionsIntVectorGenerator = new();
+string binaryReaderExtensionsIntVectorCode = binaryReaderExtensionsIntVectorGenerator.Generate();
+
+File.WriteAllText(Path.Combine(baseDirectory, "Extensions", "BinaryReaderExtensions.IntVector.g.cs"), binaryReaderExtensionsIntVectorCode);
