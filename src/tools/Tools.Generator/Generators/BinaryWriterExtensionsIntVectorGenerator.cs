b12f8d95865e52793a065d529e8ff25a8088e7cd
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-12-10 22:26:25 +0100

    Implement BinaryWriterExtensionsIntVectorGenerator

---
+++ src/tools/Tools.Generator/Generators/BinaryWriterExtensionsIntVectorGenerator.cs
@@ -0,0 +1,53 @@
+﻿using Tools.Generator.Internals;
+
+namespace Tools.Generator.Generators;
+
+internal sealed class BinaryWriterExtensionsIntVectorGenerator
+{
+	private readonly string[] _typeNames =
+	[
+		"sbyte",
+		"byte",
+		"short",
+		"ushort",
+		"int",
+		"uint",
+		"long",
+		"ulong",
+	];
+	private readonly string[] _components = ["X", "Y", "Z", "W"];
+
+	public string Generate()
+	{
+		CodeWriter codeWriter = new();
+
+		codeWriter.WriteLine(GeneratorConstants.Header);
+		codeWriter.WriteLine();
+		codeWriter.WriteLine("using Detach.Numerics;");
+		codeWriter.WriteLine();
+		codeWriter.WriteLine("namespace Detach.Extensions;");
+		codeWriter.WriteLine();
+		codeWriter.WriteLine("public static partial class BinaryWriterExtensions");
+		codeWriter.StartBlock();
+
+		for (int i = 2; i <= 4; i++)
+		{
+			string intVectorTypeName = $"IntVector{i}";
+
+			foreach (string builtInTypeName in _typeNames)
+			{
+				codeWriter.WriteLine($"public static void Write(this BinaryWriter binaryWriter, {intVectorTypeName}<{builtInTypeName}> value)");
+				codeWriter.StartBlock();
+				for (int j = 0; j < i; j++)
+					codeWriter.WriteLine($"binaryWriter.Write(value.{_components[j]});");
+				codeWriter.EndBlock();
+
+				codeWriter.WriteLine();
+			}
+		}
+
+		codeWriter.EndBlock();
+
+		return codeWriter.ToString();
+	}
+}
--- src/tools/Tools.Generator/Program.cs
+++ src/tools/Tools.Generator/Program.cs
@@ -4,5 +4,8 @@ const string baseDirectory = @"C:\Users\NOAH\source\repos\detach\src\libs\Detach
 
 BinaryReaderExtensionsIntVectorGenerator binaryReaderExtensionsIntVectorGenerator = new();
 string binaryReaderExtensionsIntVectorCode = binaryReaderExtensionsIntVectorGenerator.Generate();
-
 File.WriteAllText(Path.Combine(baseDirectory, "Extensions", "BinaryReaderExtensions.IntVector.g.cs"), binaryReaderExtensionsIntVectorCode);
+
+BinaryWriterExtensionsIntVectorGenerator binaryWriterExtensionsIntVectorGenerator = new();
+string binaryWriterExtensionsIntVectorCode = binaryWriterExtensionsIntVectorGenerator.Generate();
+File.WriteAllText(Path.Combine(baseDirectory, "Extensions", "BinaryWriterExtensions.IntVector.g.cs"), binaryWriterExtensionsIntVectorCode);
