504e45234762a61a5324e426d3b20d939efe9b4f
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2025-03-26 16:03:32 +0100

    Set up snapshot tests for some algorithms

--- src/tests/Detach.Tests.Snapshot/Tests/CollisionAlgorithmTests.cs
+++ src/tests/Detach.Tests.Snapshot/Tests/CollisionAlgorithmTests.cs
@@ -1,21 +1,36 @@
 ﻿using CollisionFormats;
+using CollisionFormats.Execution;
 using CollisionFormats.Model;
 
 namespace Detach.Tests.Snapshot.Tests;
 
 [TestClass]
+[UsesVerify]
 public partial class CollisionAlgorithmTests
 {
-	[TestMethod]
-	public async Task CircleCircle()
+	[DataTestMethod]
+	[DataRow("Detach.Collisions.Geometry2D.CircleCircle(Circle,Circle).txt")]
+	[DataRow("Detach.Collisions.Geometry2D.CircleRectangle(Circle,Rectangle).txt")]
+	[DataRow("Detach.Collisions.Geometry2D.ClosestPointOnLine(Vector2,LineSegment2D).txt")]
+	[DataRow("Detach.Collisions.Geometry3D.ClosestPointOnLine(Vector3,LineSegment3D).txt")]
+	public Task TestCollisionAlgorithmScenarios(string fileName)
 	{
-		//CollisionAlgorithm algorithm = CollisionAlgorithmSerializer.DeserializeText(await File.ReadAllTextAsync(Path.Combine("Resources", "Detach.Collisions.Geometry2D.CircleCircle.txt")));
+#pragma warning disable CA1849
+		CollisionAlgorithm algorithm = CollisionAlgorithmSerializer.DeserializeText(File.ReadAllText(Path.Combine("Resources", fileName)));
+#pragma warning restore CA1849
 
-		//Verify(code);
+		IExecutableCollisionAlgorithm? executableCollisionAlgorithm = ExecutableCollisionAlgorithms.All.FirstOrDefault(ea => ea.Name == algorithm.MethodSignature);
+		if (executableCollisionAlgorithm == null)
+			Assert.Fail($"The algorithm {algorithm.MethodSignature} was not found.");
+
+		List<Result> results = algorithm.Scenarios.ConvertAll(scenario =>
+		{
+			ExecutionResult executionResult = executableCollisionAlgorithm.Execute(scenario.Arguments);
+			return new Result(scenario.Arguments.ConvertAll(a => a.ToString() ?? "<NULL>"), executionResult);
+		});
+
+		return Verify(results);
 	}
 
-	// private async Task VerifyAsync()
-	// {
-	// 	return Verifier.Verify(driver).UseDirectory(Path.Combine("..", "snapshots"));
-	// }
+	private sealed record Result(List<string> Arguments, ExecutionResult ExecutionResult);
 }
