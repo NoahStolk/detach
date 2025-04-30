03c9ddce2a8e4d2ff3b26872714b47738bac25f8
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2025-04-30 12:56:27 +0200

    Update SphereCylinder with intersection point

--- src/libs/Detach/Collisions/Geometry3D.Sphere.cs
+++ src/libs/Detach/Collisions/Geometry3D.Sphere.cs
@@ -204,8 +204,10 @@ public static partial class Geometry3D
 		return false;
 	}
 
-	public static Vector3? SphereCylinderNormal(Sphere sphere, Cylinder cylinder)
+	public static bool SphereCylinder(Sphere sphere, Cylinder cylinder, out IntersectionResult result)
 	{
+		result = default;
+
 		// Check intersection with bottom circle
 		Vector2 sphereXz = new(sphere.Center.X, sphere.Center.Z);
 		Vector2 cylinderXz = new(cylinder.BottomCenter.X, cylinder.BottomCenter.Z);
@@ -220,11 +222,17 @@ public static partial class Geometry3D
 
 		// Check bottom circle
 		if (sphereMinY <= cylinderMinY && sphereMaxY >= cylinderMinY && distanceSquaredXz <= radiusSum * radiusSum)
-			return -Vector3.UnitY; // Normal points away from cylinder
+		{
+			result = new IntersectionResult(-Vector3.UnitY, sphere.Center + Vector3.UnitY * (sphere.Radius - (cylinderMinY - sphereMinY)));
+			return true;
+		}
 
 		// Check top circle
 		if (sphereMaxY >= cylinderMaxY && sphereMinY <= cylinderMaxY && distanceSquaredXz <= radiusSum * radiusSum)
-			return Vector3.UnitY; // Normal points away from cylinder
+		{
+			result = new IntersectionResult(Vector3.UnitY, sphere.Center - Vector3.UnitY * (sphere.Radius - (sphereMaxY - cylinderMaxY)));
+			return true;
+		}
 
 		// Check side surface
 		if (sphereMaxY >= cylinderMinY && sphereMinY <= cylinderMaxY)
@@ -232,6 +240,8 @@ public static partial class Geometry3D
 			float distanceXz = MathF.Sqrt(distanceSquaredXz);
 			if (distanceXz <= cylinder.Radius + sphere.Radius)
 			{
+				Vector3 normal, intersectionPoint;
+
 				// Special case: sphere is centered on cylinder's axis
 				const float epsilon = 0.001f;
 				if (distanceXz is > -epsilon and < epsilon)
@@ -239,15 +249,21 @@ public static partial class Geometry3D
 					// Return normal of the closest circle
 					float distanceToBottom = Math.Abs(sphere.Center.Y - cylinderMinY);
 					float distanceToTop = Math.Abs(sphere.Center.Y - cylinderMaxY);
-					return distanceToBottom < distanceToTop ? -Vector3.UnitY : Vector3.UnitY;
+					normal = distanceToBottom < distanceToTop ? -Vector3.UnitY : Vector3.UnitY;
+					intersectionPoint = sphere.Center - normal * (sphere.Radius - Math.Min(distanceToBottom, distanceToTop));
+					result = new IntersectionResult(normal, intersectionPoint);
+					return true;
 				}
 
 				// Calculate normal pointing from cylinder center to sphere
-				Vector3 normal = new(sphere.Center.X - cylinder.BottomCenter.X, 0, sphere.Center.Z - cylinder.BottomCenter.Z);
-				return Vector3.Normalize(normal);
+				normal = new Vector3(sphere.Center.X - cylinder.BottomCenter.X, 0, sphere.Center.Z - cylinder.BottomCenter.Z);
+				normal = Vector3.Normalize(normal);
+				intersectionPoint = sphere.Center - normal * (sphere.Radius - (cylinder.Radius - distanceXz));
+				result = new IntersectionResult(normal, intersectionPoint);
+				return true;
 			}
 		}
 
-		return null;
+		return false;
 	}
 }
